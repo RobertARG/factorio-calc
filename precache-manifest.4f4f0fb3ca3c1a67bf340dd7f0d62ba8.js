b5d6562eaa54a3a6dde14b663325abb64eb9cdf6
Pedro Calogero <calogero.pvj@gmail.com>
2020-03-15 21:22:40 -0300

    Updates

---
+++ precache-manifest.4f4f0fb3ca3c1a67bf340dd7f0d62ba8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15708be7ab9b59f7ad90973cddfd4d01",
+    "url": "/factorio-calc/index.html"
+  },
+  {
+    "revision": "57e2eb41c0e52cdb367c",
+    "url": "/factorio-calc/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "51f8fbe7adb521e84418",
+    "url": "/factorio-calc/static/js/2.8d2b488b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/factorio-calc/static/js/2.8d2b488b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57e2eb41c0e52cdb367c",
+    "url": "/factorio-calc/static/js/main.ca26ad28.chunk.js"
+  },
+  {
+    "revision": "f91c5532ec6402ff0dae",
+    "url": "/factorio-calc/static/js/runtime-main.b0b052d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/factorio-calc/static/media/logo.5d5d9eef.svg"
+  }
+]);
